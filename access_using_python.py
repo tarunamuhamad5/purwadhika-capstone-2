81081010adeba10983f4c3843b115e53ad9f81e7
Muhamad Taruna <79249844+tarunamuhamad5@users.noreply.github.com>
2025-06-04 21:52:21 +0700

    adding function to access my sql using python

---
+++ access_using_python.py
@@ -0,0 +1,21 @@
+import mysql.connector
+import pandas as pd
+
+sql_enggine = mysql.connector.connect(
+    host= "localhost",
+    port=3306,
+    user='root',
+    password="abcde12345",
+    database="employees"
+)
+
+cursor = sql_enggine.cursor()
+
+query = "select * from employees where gender ='M'"
+
+cursor.execute(query)
+
+result = cursor.fetchall()
+
+df = pd.DataFrame(result, columns = cursor.column_names)
+print(df)
